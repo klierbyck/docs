ed83f21cf79df6a7e01012c0a8ad5ca6d31a078b
xuw-i(10027568) <xuw-i@glodon.com>
2019-11-12 09:27:55 +0800

    update docs

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,9 @@
-#!/usr/bin/env sh
-
 set -e
 
 npm run docs:build
 
-cd docs/.vuepress/dist
+git add .
 
-git init
-git add -A
-git commit -m 'deploy'
+git commit -m 'update docs'
 
-# 如果是发布到自定义域名
- echo 'docs.klierbyck.com' > CNAME
-
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
-cd -
+git push
